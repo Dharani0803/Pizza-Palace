d15a8a6c4185cab2fefca53d4082b6775d10879a
Dharani0803 <dd1452327@gmail.com>
2026-06-02 15:32:31 +0530

    Updated

--- frontend/src/components/Pizzaskeleton.js
+++ frontend/src/components/Pizzaskeleton.js
@@ -3,10 +3,35 @@ import "react-loading-skeleton/dist/skeleton.css";
 
 function PizzaSkeleton() {
   return (
-    <div className="p-4 bg-white rounded-md shadow">
-      <Skeleton height={150} />
-      <Skeleton count={2} className="mt-2" />
-      <Skeleton width={80} />
+    <div className="p-5">
+      
+      {/* Banner Skeleton */}
+      <Skeleton height={220} borderRadius={12} />
+
+      {/* Section Title */}
+      <div className="mt-6">
+        <Skeleton width={200} height={30} />
+      </div>
+
+      {/* Pizza Cards */}
+      <div className="flex gap-5 mt-5 overflow-hidden">
+        
+        {[1,2,3].map((item) => (
+          <div
+            key={item}
+            className="min-w-[340px] rounded-xl overflow-hidden"
+          >
+            <Skeleton height={250} />
+
+            <div className="mt-3">
+              <Skeleton width={180} height={25} />
+              <Skeleton count={2} />
+              <Skeleton width={100} height={25} />
+            </div>
+          </div>
+        ))}
+
+      </div>
     </div>
   );
 }
